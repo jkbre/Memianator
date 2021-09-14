63f0380d274ca758d3a18fb94395b785c6a0e90d
jkbre <jakubre596@student.polsl.pl>
2021-09-14 20:35:15 +0200

    Action Buttons Back, Next
    App shows meme from list of 10 downloaded memes from r/memes/hot. User could switch between downloaded memes.

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import PyQt5.QtWidgets as qtw
 import PyQt5.QtGui as qtg
 from PyQt5.QtCore import Qt as qtc
 import sys
-import random
 import requests
 import praw
 
 class RedditApi():
     def __init__(self):
         self.api_ids()
-        self.source_set()
-        self.meme_grapper()
+        self.meme_init()
+        self.dev = 'on'
     
     def api_ids(self):
         self.reddit = praw.Reddit(client_id='wLiJD2nHgMwjBdwK9Yq_kQ', \
@@ -19,28 +18,41 @@ class RedditApi():
                      username='dewfdcs2', \
                      password='Scholar/Compile/Nurture3')
     
-    def source_set(self):
+    def meme_init(self, rang = 'hot', num = 10):
         self.subreddit = self.reddit.subreddit('memes')
+        self.limit_num = num
+        self.all_subs = []
+        self.sub_idx = 0
 
-    def meme_grapper(self):
-        all_subs = []
+        if rang == 'hot':
+            meme_list = self.subreddit.hot(limit=self.limit_num)
 
-        hot = self.subreddit.hot(limit=50)
+        for submission in meme_list:
+            self.all_subs.append(submission)
 
-        for submission in hot:
-            all_subs.append(submission)
+        self.url_id = self.all_subs[self.sub_idx].id
+        self.url_title = self.all_subs[self.sub_idx].title
+        self.url_address = self.all_subs[self.sub_idx].url
 
-        random_sub = random.choice(all_subs)
-
-        self.url_title = random_sub.title
-        self.url_address = random_sub.url
+    def meme_switcher(self, itt):
+        self.sub_idx = self.sub_idx + itt
+        if self.sub_idx < 0:
+            self.sub_idx = 0
+        elif self.sub_idx > self.limit_num-1:
+            self.sub_idx = self.limit_num-1
+            if self.dev == 'on':
+                print('zatrzymało')
+        else:
+            self.url_id = self.all_subs[self.sub_idx].id
+            self.url_title = self.all_subs[self.sub_idx].title
+            self.url_address = self.all_subs[self.sub_idx].url
 
 class MainWindow(qtw.QWidget):
     def __init__(self):
         super().__init__()
 
         self.api = RedditApi()
-
+        self.meme_label = qtw.QLabel()
         self.setWindowTitle('Memianator')
         self.setLayout(qtw.QVBoxLayout())
         self.keypad()
@@ -52,15 +64,11 @@ class MainWindow(qtw.QWidget):
         container.setLayout(qtw.QGridLayout())
 
         # Meme
-        image = qtg.QImage()
-        image.loadFromData(requests.get(self.api.url_address).content)
-
-        image_label = qtw.QLabel()
-        pixmap = qtg.QPixmap(image)
-        smaller_pixmap = pixmap.scaled(400, 400, qtc.KeepAspectRatio, qtc.SmoothTransformation)
-
-        image_label.setPixmap(smaller_pixmap)
-
+        meme = qtg.QImage()
+        meme.loadFromData(requests.get(self.api.url_address).content)
+        pixmap = qtg.QPixmap(meme)
+        smaller_pixmap = pixmap.scaled(400, 400, qtc.KeepAspectRatio, qtc.SmoothTransformation)       
+        self.meme_label.setPixmap(smaller_pixmap)
 
         # Buttons        
         btn_back = qtw.QPushButton('Back', clicked = self.back_meme)
@@ -69,16 +77,34 @@ class MainWindow(qtw.QWidget):
         
 
         # Adding the buttons to the layout
-        container.layout().addWidget(image_label,0,0,1,6)
+        container.layout().addWidget(self.meme_label,0,0,1,6)
         container.layout().addWidget(btn_back,1,0,1,2)
         container.layout().addWidget(btn_next,1,4,1,2)
         container.layout().addWidget(btn_save,1,2,1,2)
         self.layout().addWidget(container)
 
     def back_meme(self):
-        pass
+        self.api.meme_switcher(-1)
+
+        meme = qtg.QImage()
+        meme.loadFromData(requests.get(self.api.url_address).content)
+
+        pixmap = qtg.QPixmap(meme)
+        smaller_pixmap = pixmap.scaled(400, 400, qtc.KeepAspectRatio, qtc.SmoothTransformation)
+               
+        self.meme_label.setPixmap(smaller_pixmap)
+
     def next_meme(self):
-        pass
+        self.api.meme_switcher(1)
+
+        meme = qtg.QImage()
+        meme.loadFromData(requests.get(self.api.url_address).content)
+
+        pixmap = qtg.QPixmap(meme)
+        smaller_pixmap = pixmap.scaled(400, 400, qtc.KeepAspectRatio, qtc.SmoothTransformation)
+                
+        self.meme_label.setPixmap(smaller_pixmap)
+        
     def save_meme(self):
         pass
 
